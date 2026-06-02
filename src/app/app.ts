f1d07622c47e5dd34ed10750971be55f570dfe39
C4rseto <silvaoctubre16@gmail.com>
2026-06-02 01:33:02 -0500

    wasa

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('deepl_arabica');
+  pantallaActual: string = 'login'; 
+
+  usuario: string = '';
+  password: string = '';
+
+  iniciarSesion() {
+    if(this.usuario && this.password) {
+      this.pantallaActual = 'dashboard';
+    } else {
+      alert('Por favor ingresa tu usuario y contraseña.');
+    }
+  }
+
+  cerrarSesion() {
+    this.pantallaActual = 'login';
+    this.usuario = '';
+    this.password = '';
+  }
+
+  cambiarPantalla(pantalla: string) {
+    this.pantallaActual = pantalla;
+  }
 }
